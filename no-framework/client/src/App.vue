2c3b388ab49c5b262905c0aa881fcaf6fc34dda4
simplesolutionist <aymanali180@gmail.com>
2020-10-10 21:39:09 -0400

    basic design for feed

---
+++ no-framework/client/src/App.vue
@@ -0,0 +1,21 @@
+  
+<template>
+  <section class="section">
+    <div class="container">
+      <Nav />
+      <router-view/>
+    </div>
+  </section>
+</template>
+
+<script>
+import Nav from "@/components/Nav";
+export default {
+    components: {
+        Nav
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
