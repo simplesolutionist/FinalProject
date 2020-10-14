aace76a24dc0c174eabdd67686d661c6aaf84902
simplesolutionist <aymanali180@gmail.com>
2020-10-14 12:53:25 -0400

    update client-side

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/login', name: 'Login', component: Login },
   {
-    path: '/feed',
-    name: 'Feed',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Feed.vue')
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
     path: '/about',
@@ -20,7 +17,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+  }
 ]
 
 const router = new VueRouter({
--- no-framework/client/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is <i>Our</i> about page</h1>
-  </div>
-</template>
