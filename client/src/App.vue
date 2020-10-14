fa3d238a88f34814d305234c49c47cbbcc40ff85
simplesolutionist <aymanali180@gmail.com>
2020-10-14 17:57:10 -0400

    nav bar updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <div id="app">
-    <Nav />
-    <router-view/>
+  <section class="section">
+    <div class="container">
+      <Nav />
+      <router-view/>
+      <div :style="{'background-image': 'url(https://cdn.dribbble.com/users/230290/screenshots/4035613/exe.jpg)'}"></div>
 
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,8 +1,70 @@
 <template>
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://cdn.dribbble.com/users/230290/screenshots/4035613/exe.jpg" height="300">
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      
+        <router-link to="/" class="navbar-item">Home</router-link>
+      <router-link to="/about" class="navbar-item">About</router-link>
+        <router-link to="/about" class="navbar-item">Exercises</router-link>
+        <router-link to="/about" class="navbar-item">Progress</router-link>
+      <router-link to="/about" class="navbar-item">Achievements</router-link>
+            <router-link to="/about" class="navbar-item">Friends</router-link>
+
+
+
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            Contact
+          </a>
+          <a class="navbar-item">
+            Newsletter
+          </a>
+          <a class="navbar-item">
+            Settings
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
     </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
 </template>
 
 
