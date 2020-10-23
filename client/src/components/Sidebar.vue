9a3794d2e99f20b54b9962876fb958bd7b13e62b
simplesolutionist <aymanali180@gmail.com>
2020-10-22 20:56:50 -0400

    added basic design elements for friends page

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,57 @@
+<template>
+<form>
+<nav class="panel">
+  <p class="panel-heading">
+    Suggestions For You
+  </p>
+    <p class="panel-tabs">
+    <a class="is-active">Suggestions</a>
+    <a>Friends</a>
+    <a>Requests</a>
+    <a>See All</a>
+  </p>
+  <a class="panel-block is-active">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    bulma
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    marksheet
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    minireset.css
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+    </span>
+    jgthms.github.io
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch" aria-hidden="true"></i>
+    </span>
+    daniellowtw/infboard
+  </a>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch" aria-hidden="true"></i>
+    </span>
+    mojs
+  </a>
+  
+  <div class="panel-block">
+    <button class="button is-link is-outlined is-fullwidth">
+      Add Friend
+    </button>
+  </div>
+</nav>
+</form>
+</template>
